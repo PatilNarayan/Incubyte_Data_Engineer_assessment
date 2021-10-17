75d597c5d366b3af1e7db2e5b8abdb610dbd043b
Patil Narayan <70795556+PatilNarayan@users.noreply.github.com>
2021-10-17 12:53:05 +0530

    Comments added

--- TestDrivenDevelopment.py
+++ TestDrivenDevelopment.py
@@ -6,17 +6,21 @@ Created on Sun Oct 17 12:28:00 2021
 @author: patilnarayan
 """
 
+# importing necessary libraries
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 
 sc = SparkContext('local','example')  # if using locally
 sqlContext = SQLContext(sc)
 
-
+# fetching tables
 df=sqlContext.sql("select * from hospital.patients")
 
+# visualization Data
 df.show()
 
-
+#customers as per Country 
+# India patients extracting
+# change in 'IND' if you want any other country data
 df.where(df.Country=='IND').show()
     
